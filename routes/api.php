e5481214c477a7eb64e85b05422b122c914c8bf6
lionheartsq <lujogarpin78@gmail.com>
2024-08-28 08:19:01 -0500

    Add endpoint ofertas
    Add endpoint ofertas

--- routes/api.php
+++ routes/api.php
@@ -153,6 +153,7 @@ Route::group(['prefix' => 'auth'], function () {
      Route::put("asociados/activate", "Tb_asociadosController@activate");
      Route::get("asociados/selectasociados/{id}", "Tb_asociadosController@indexOne");
      Route::get("asociados/detallado/{id}", "Tb_asociadosController@indexOneDetalle");
+     Route::get("asociados/ofertas/{id}", "Tb_asociadosController@indexOneOfertas");
 
      Route::get("asociados_finca", "Tb_asociados_fincasController@index");
      Route::post("asociados_finca/store", "Tb_asociados_fincasController@store");
