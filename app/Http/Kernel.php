660bd6c7342ba5ec90fa7f3873e24753523be34e
lionheartsq <lujogarpin78@gmail.com>
2024-11-27 09:42:39 -0500

    Update cors
    Update cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,19 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'api' => [
+            \Fruitcake\Cors\HandleCors::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
 
+        /*
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        */
+
     ];
 
     /**
