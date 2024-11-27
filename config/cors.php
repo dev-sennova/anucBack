ccf9dad700da1cec9ec4fa397b67f9ed7d4a36ae
lionheartsq <lujogarpin78@gmail.com>
2024-11-27 09:05:12 -0500

    Adding cors
    Adding cors

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,10 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => false,
-
-    'max_age' => false,
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],  // Permite todos los métodos, incluyendo OPTIONS
+    'allowed_origins' => ['*'],  // Cambia por el dominio adecuado para producción, por ejemplo: ['https://anuc.hourclient.com']
+    'allowed_headers' => ['*'],  // Permite todos los encabezados
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => false,
-
 ];
